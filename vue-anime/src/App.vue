105a47b1605219f2ff00a47e2c876154548eb783
Anvay Bhanap <anvay.bhanap@gmail.com>
2022-01-12 16:40:00 -0800

    installed and added vue packages

---
+++ vue-anime/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300&display=swap" rel="stylesheet">
+  <div class="app">
+    <header>
+      <h1>The<strong>Anime</strong>Database</h1>
+    </header>
+  </div> 
+</template>
+
+<script>
+
+
+export default {
+  name: 'app',
+  
+  
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Roboto Condensed', sans-serif;
+}
+
+a {
+  text-decoration: none;
+}
+#app {
+  
+}
+
+header {
+  padding-top: 50px;
+  padding-bottom: 50px;
+
+}
+header h1 {
+  color: #888;
+  font-size: 42px;
+  font-weight: 400;
+  text-align: center;
+  text-transform: uppercase;
+}
+header h1 strong {
+  color: black;
+}
+</style>
